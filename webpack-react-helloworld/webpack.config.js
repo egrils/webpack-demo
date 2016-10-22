b39af514ba89bdff82e0cc80b4df733e5b3eeeff
egrils <angela335889390@gmail.com>
2016-10-22 20:18:53 +0800

    init webpack-react-hello src

---
+++ webpack-react-helloworld/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module:{
+        loaders:[{
+            test: /\.js$/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015','react']
+            }
+        }]
+    }
+};
+
+
